d32388b0dd1efe8ab21821063a01cfc21cc39346
Renan <renan.flores@agencia2bdigital.com.br>
2022-03-17 23:19:44 -0300

    fix: add cors

--- src/services/axios/AxiosInstance.ts
+++ src/services/axios/AxiosInstance.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const vtex = axios.create({
   baseURL: window.location.origin,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': '*'
+  }
 });
+
 
 export default vtex;
